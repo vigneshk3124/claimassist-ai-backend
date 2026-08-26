049e3cb61ea38f3158dee588e0c337d648967f3f
Vignesh K <vignesh.kuppusamy@tvs.in>
2026-08-26 11:24:49 +0530

    testing

--- src/main/java/com/tvs/claimassist/backend/config/SecurityConfig.java
+++ src/main/java/com/tvs/claimassist/backend/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+//                .csrf(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
